2b867cd58a04774eb184f70997a22e344c66625b
Niranjan Ramadas <nrbramadas@gmail.com>
2015-10-09 17:21:33 -0700

    The initial bits of the app

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
+import requests
 from flask import Flask, render_template
 app = Flask(__name__)
+
+app.debug = True
+
+ENDPOINT = "http://mgoblog.com/mobileservices"
 
 @app.route("/")
 def main():
     return render_template("main.html")
+
+@app.route("/homepage/<int:page>")
+def fetch_homepage(page):
+    return requests.get(ENDPOINT + "/node.json?parameters[sticky]=0&parameters[promote]=1&page={page}".format(page=page)).content
+
+@app.route("/boards/<int:page>")
+def fetch_boards(page):
+    return requests.get(ENDPOINT + "/node.json?parameters[sticky]=0&page={page}".format(page=page)).content
+
+@app.route("/post/<int:post_id>")
+def fetch_post(post_id):
+    return requests.get(ENDPOINT + "/node/{post_id}.json".format(post_id=post_id)).content
+
+@app.route("/comments/<int:post_id>")
+def fetch_comments(post_id):
+    return requests.get(ENDPOINT + "/node/{post_id}/comments.json".format(post_id=post_id)).content
 
 if __name__ == "__main__":
     app.run()
